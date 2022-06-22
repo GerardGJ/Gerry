0bd26289c3a163bcddf58bdde56f09ee37cd8e47
Gerard Gomez Jorba <58191927+GerardGJ@users.noreply.github.com>
2022-06-22 17:31:31 +0200

    Update sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -1,3 +1,5 @@
+from pickle import NONE
+from itertools import chain 
 import sys
 import numpy as np
 #Possible input:
@@ -16,11 +18,47 @@ class Missing_value(object):
     """This class represents the missing value
     """
     def __init__(self,value:int,i:int,j:int):
-        self.value = value
+        self.__value__ = value
         self.position_row = i
         self.position_column = j
         self.possible_number = set()
     
+    def set_value(self,value) -> None:
+        self.__value__ = value
+    
+    def get_value(self) -> int:
+        return self.__value__
+
+    def __repr__(self) -> str:
+        return str(self.get_value())
+    
+class Value_sudoku():
+    def __init__(self,value, i, j):
+        self.given_value = None
+        self.missing_value = None
+        self.set_value(value,i,j)
+
+    def __repr__(self):
+        if self.given_value == None:
+            return str(self.missing_value.get_value())
+        return str(self.given_value)
+
+    def set_value(self,value,i,j):
+        if value == '0':
+            self.missing_value = Missing_value(value,i,j)
+            self.given_value = None
+        else:
+            self.given_value = value
+            self.missing_value = None
+    
+    def from_missing_to_given(self,value):
+        self.given_value = value
+        self.missing_value = None
+    
+    def get_value(self):
+        if self.given_value == None:
+            return int(self.missing_value.get_value())
+        return int(self.given_value)
 
 class Sudoku(object):
 
@@ -34,44 +72,36 @@ class Sudoku(object):
 
         Args:
             path_to_sudoku (str): path to the file with the sudoku
-            sudoku (np.array): all zeros numpy array of 9 x 9 dimensions
-
         Returns:
             np.array: the sudoku numpy array with the given numbers as strings and the missing as int
             list: x and y position of the missing values
         """
         sudoku = [ [0]*9 for i in range(9)] #first generate the table (np.zeros((9,9)).astype(int))
-
         missing_positions = []
+
         with open(path_to_sudoku) as file_with_sudoku: #Here we open the file:
             for i,line in enumerate(file_with_sudoku): #Get the number of the line
                 for j,num in enumerate(line.split()): #Get the number of the column and get the value in that position in the sudoku 
-                    if num != '0':    
-                        sudoku[i][j] = num #Change the value of 0 to the value in the sudoku
-                    else:
+                    sudoku[i][j] = Value_sudoku(num,i,j)
+                    if num == '0': 
                         missing_positions.append((i,j)) #If is a 0 save the position
-                        sudoku[i][j] = Missing_value(0,i,j)
+
         return sudoku, missing_positions
 
     def get_column(self,j:int) -> list:
-        col = [row[j] for row in self.sudoku]
-        for i in range(9):
-            if(type(col[i]) == Missing_value):
-                col[i] = col[i].value
+        col = [row[j].get_value() for row in self.sudoku]
         return col
     
-    def get_row(self,i:int) -> list:
-        row = self.sudoku[i]
+    def get_row(self,row_number:int) -> list:
+        row = self.sudoku[row_number].copy()
         for i in range(9):
-            if(type(row[i]) == Missing_value):
-                row[i] = row[i].value
+            row[i] = row[i].get_value()
         return row
     
-    def make_square(self,i,j):
+    def make_square(self,i:int,j:int) -> list:
         """
         This function generates the square corresponding to that possition
         
-        :param table: numpy array representing the sudoku  
         :param i: int row index
         :param j: int column index
         :return: list correponding to the numbers in the square
@@ -81,67 +111,60 @@ class Sudoku(object):
             if j in range(0,3): #Left square
                 for x in range(0,3):
                     for y in range(0,3):
-                        square.append(self.sudoku[x][y])
+                        square.append(self.sudoku[x][y].get_value())
             elif j in range(3,6): #Middel square
                 for x in range(0,3):
                     for y in range(3,6):
-                        square.append(self.sudoku[x][y])
+                        square.append(self.sudoku[x][y].get_value())
             else: #Right Square
                 for x in range(0,3):
                     for y in range(6,9):
-                        square.append(self.sudoku[x][y])
+                        square.append(self.sudoku[x][y].get_value())
         elif i in range(3,6): #Middel Squares
             if j in range(0,3): #Left Squeare
                 for x in range(3,6):
                     for y in range(0,3):
-                        square.append(self.sudoku[x][y])
+                        square.append(self.sudoku[x][y].get_value())
             elif j in range(3,6): #Middel Square
                 for x in range(3,6):
                     for y in range(3,6):
-                        square.append(self.sudoku[x][y])
+                        square.append(self.sudoku[x][y].get_value())
             else: #Right Square
                 for x in range(3,6):
                     for y in range(6,9):
-                        square.append(self.sudoku[x][y])
+                        square.append(self.sudoku[x][y].get_value())
         else: #Bottom Squares
             if j in range(0,3): #Left Squre
                 for x in range(6,9):
                     for y in range(0,3):
-                        square.append(self.sudoku[x][y])
+                        square.append(self.sudoku[x][y].get_value())
             elif j in range(3,6): #Middel Square
                 for x in range(6,9):
-                    for y in range(4,6):
-                        square.append(self.sudoku[x][y])
+                    for y in range(3,6):
+                        square.append(self.sudoku[x][y].get_value())
             else: #Right Square
                 for x in range(6,9):
                     for y in range(6,9):
-                        square.append(self.sudoku[x][y])
-        for i in range(9):
-            if(type(square[i]) == Missing_value):
-                square[i] = square[i].value
+                        square.append(self.sudoku[x][y].get_value())
         return square
 
     def evaluate_missing_positions(self):
         """This function will go through each missing position and evaluate its missing value
         """
-        
         while True:
             position_filled = []
             for i,position in enumerate(self.missing_positions):
-                print(self.sudoku)
                 row,column = position
                 missing = self.sudoku[row][column]
-                #print(self.sudoku)
-                #print("is",self.sudoku[row][column])
                 self.evaluate(row,column)
-                if len(missing.possible_number) == 1:
-                    self.sudoku[row][column] = missing.possible_number[0]
+                if len(missing.missing_value.possible_number) == 1:
+                    self.sudoku[row][column].from_missing_to_given(missing.missing_value.possible_number[0])
                     position_filled.append(i)
             if len(position_filled) >= 1:
                 del self.missing_positions[position_filled]
             else: 
                 return None
-    
+  
     def evaluate(self, i,j) -> None:
         """
         This function updates the possible numbers
@@ -163,27 +186,63 @@ class Sudoku(object):
         for num in square:
             if num != 0:
                 not_possibilities.append(num)
-        self.possible_number = set(not_possibilities).difference(list(range(10)))
+        not_possibilities = set(not_possibilities)
+        self.sudoku[i][j].missing_value.possible_number = not_possibilities.difference(set(range(1,10)))
+
+    def inspection(self):
+        change_done = True
+        while change_done:
+            change_done = False
+            for row_number in range(9):
+                row = self.get_row(row_number)
+                if 0 in row:
+                    row_missing_index = list(np.where(np.array(row) == 0)[0])
+                    missing_in_row = []
+                    for col_number in row_missing_index:
+                        col = self.get_column(col_number)
+                        sqr = self.make_square(row_number,col_number)
+                        row_test = self.get_row(row_number)
+                        numbers_given = set(col + sqr + row_test)
+                        numbers_given.remove(0)
+                        missing_in_row.append(list(set(range(1,10)).difference(numbers_given)))
+                    change_done = True
+                    if row_number == 4:
+                        print(missing_in_row)
+                    while change_done:
+                        change_done = False
+                        for x in range(len(missing_in_row)):
+                            test = set(missing_in_row.pop(0)) 
+                            rest = missing_in_row.copy()
+                            rest = set(list(chain.from_iterable(rest)))
+                            aveliable = test.difference(rest)
+                            if len(aveliable) == 1:
+                                index_col = row_missing_index.pop(x) 
+                                self.sudoku[row_number][index_col].from_missing_to_given(list(aveliable)[0])
+                                change_done = True
+                            else: 
+                                missing_in_row.append(test)
+                
 
     def solve(self,index):
         """
         This function solves the sudoku
 
         :param index: int index of the positions list
-        :return: list[np.array(np.array(int)),bool] The sudoku table and a bool True when the table i correct and full False try a different number
+        :return: list, bool The sudoku table and a bool True when the table i correct and full False try a different number
         """
         if index == len(self.missing_positions): #base case (when we locked all the positions)
             return self.sudoku,True
         else:
             flag_return = False
             i,j = self.missing_positions[index] #look at one position in the list
-            for x in self.sudoku[i,j].possible_number:
+            for x in self.sudoku[i][j].missing_value.possible_number:
                 self.sudoku[i][j].value = x #Try one number
                 if self.check(self.sudoku,i,j): #If that number fits correcly
-                    self.sudoku,flag_return = solve(index+1) #Look at the next coord in the list
+                    self.sudoku,flag_return = self.solve(index+1) #Look at the next coord in the list
                     if flag_return: #If the self.sudoku is posible 
                         break #sotp trying numbers
-            if flag_return: #if sudoku possible
+            if flag_return: #if sudoku possibleç
+                self.sudoku[i][j].from_missing_to_given(self.sudoku[i][j].get_value())
                 return self.sudoku,True #return the sudoku
             self.sudoku[i][j].value = 0 #Backtracking
             return self.sudoku,False    
@@ -192,7 +251,6 @@ class Sudoku(object):
         """
         This function chakes if a number at a possition is valid
         
-        :param table: np.array with where the sudoku is stored
         :param i: int row index
         :param j: int column index 
         :retun: bool True if number is correct, False if number is incorrect
@@ -202,16 +260,28 @@ class Sudoku(object):
             return False
         if self.get_column.count(number) > 1:#Here we check the column
             return False
-        my_square = make_square(i,j) #Get the square corresponding to that number
+        my_square = self.make_square(i,j) #Get the square corresponding to that number
         if np.count_nonzero(my_square == number) > 1:
             return False
         return True
 
+    def print_sudoku(self):
+        for x in range(9):    
+            print(self.sudoku[x][0:3],"|",self.sudoku[x][3:6],"|",self.sudoku[x][6:9])
+            if x == 2 or x == 5:
+                print("-"*33)
+
 def sudoku_solver():
     my_sudoku = Sudoku(sys.argv[1])
+    my_sudoku.print_sudoku()
+    print("\n"*2)
     my_sudoku.evaluate_missing_positions()
-    #my_sudoku.solve()
-    #print(my_sudoku.sudoku)
+    my_sudoku.inspection()
+    my_sudoku.print_sudoku()
+
+
+    #my_sudoku.solve(0)
+    #my_sudoku.print_sudoku()
 
 if __name__ == "__main__":
     sudoku_solver()
